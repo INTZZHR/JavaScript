2b60e28d5aea65515841216c540b555f5324c00d
INTZZHR <106556951+INTZZHR@users.noreply.github.com>
2023-04-10 09:36:18 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,141 @@
+//atividade 1
+
+
+/*
+let i = 1
+
+while( i <= 10){
+    console.log(i++)
+}
+*/
+
+
+//atividade 2
+
+
+/*
+var n = prompt("Digite um numero");
+var total = n/2;
+
+if(n & 1){
+    alert("Impar");
+} else {
+    alert("Par");
+}
+*/
+
+
+//atividade 3
+
+
+/*
+var a = +prompt(" Informe o primeiro numero ")
+var b = +prompt(" Informe o segundo numero ")
+
+var media = ((a + b) / 2)
+    console.log("a media é: ", media)
+*/
+
+
+//atividade 4
+
+
+/*
+var Numero1 = prompt("Informe o primeiro numero")
+var Numero2 = prompt("Informe o segundo numero")
+var Numero3 = prompt("Informe o terceiro numero")
+var Numero4 = prompt("Informe o quarto numero")
+var Numero5 = prompt("Informe o quinto numero")
+
+console.log(Math.max(Numero1, Numero2, Numero3, Numero4, Numero5));
+*/
+
+
+//atividade 5
+
+/*
+var Array01 = prompt("Informe o primeiro numero")
+var Array02 = prompt("Informe o segundo numero")
+var Array03 = prompt("Informe o terceiro numero")
+var ArrayTotal = Array01+Array02+Array03;
+
+console.log("ArrayTotal: "+ArrayTotal.length);
+*/
+
+
+//atividade 6
+
+/*
+let list = ["barra", "basilisco", "football", "queixo"];
+list.splice(2, 1);
+
+console.log(list)
+*/
+
+
+//atividade 7
+
+/*
+let frase = "Frase Revertida";
+
+let inverter = frase.split("").reverse().join("");
+
+console.log(inverter);
+*/
+
+
+//atividade 8
+
+/*
+var nome = prompt("Informe um nome")
+
+let inverter = nome.split("").reverse().join("");
+
+if(inverter == nome){
+    alert("É um palíndromo")
+}
+
+else{
+    alert("Não é um plaíndromo")
+}
+
+console.log(inverter)
+*/
+
+
+//atividade 9
+
+/*
+var fatorial = +prompt("Infome um numero");
+var resultado = 1;
+for (var i = fatorial; i > 1; i--) {
+    resultado *= i;
+}
+console.log(resultado);
+*/
+
+
+//atividade 10
+
+/*
+var numero1 = 1
+var numero2 = numero1+numero1
+var numero3 = numero2+numero1
+var numero4 = numero3+numero2
+var numero5 = numero4+numero3
+var numero6 = numero5+numero4
+var numero7 = numero6+numero5
+var numero8 = numero7+numero6
+var numero9 = numero8+numero7
+
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
+console.log(numero4)
+console.log(numero5)
+console.log(numero6)
+console.log(numero7)
+console.log(numero8)
+console.log(numero9)
+*/
+
